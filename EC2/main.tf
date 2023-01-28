94bf418c2c90096a5160ab056b3cde3d59bba3a1
hchoi36 <hyunminc77@gmail.com>
2023-01-27 23:29:34 -0500

    changed ami to red hat

--- EC2/main.tf
+++ EC2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  ami = "ami-0b5eea76982371e91"
+  ami = "ami-08e637cea2f053dfa"
   instance_type = "t2.micro"
   key_name = "devops_lab"
   #vpc_security_group_ids = [sg-0237c70dcff4cd7f1]
