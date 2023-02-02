05640773dccf9569805d10f73f2d9f42aad18806
hchoi36 <hyunminc77@gmail.com>
2023-02-02 16:22:20 -0500

    changed to EKS and configured EKS terraform

--- terraform/EKS/main.tf
+++ terraform/EKS/main.tf
@@ -1,23 +1,26 @@
 module "eks" {
   source           = "terraform-aws-modules/eks/aws"
    version         = "17.24.0"
-  cluster_name     = "CloudGuard-Kubernetes"
-  cluster_version  = "1.19"
-  subnets          = module.vpc.public_subnets
-
-  vpc_id = module.vpc.vpc_id
-
+  cluster_name     = "Antra-Kubernetes"
+  cluster_version  = "1.24"
+  #subnets          = module.vpc.public_subnets
+  
+  #vpc_id = module.vpc.vpc_id
+   
+  write_kubeconfig = true
+  
   worker_groups = [
     {
       name                          = "worker-group-1"
       instance_type                 = "t2.small"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+      #additional_userdata           = "echo foo bar"
+      #additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
       asg_desired_capacity          = 2
+      enable_monitoring = false 
     },
   ]
 }
 
-output "env-dynamic-url" {
-  value = module.eks.cluster_endpoint
-}
+# output "env-dynamic-url" {
+#   value = module.eks.cluster_endpoint
+# }
