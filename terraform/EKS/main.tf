63e23c60b681a542b9c9dacc14cacca54b89e6d6
hchoi36 <hyunminc77@gmail.com>
2023-02-03 10:59:04 -0500

    uncommented cluster endpoint output

--- terraform/EKS/main.tf
+++ terraform/EKS/main.tf
@@ -4,7 +4,6 @@ module "eks" {
   cluster_name     = "Antra-Kubernetes"
   cluster_version  = "1.19"
   subnets          = ["subnet-0f78a8893b01ab3b0","subnet-0e0112694839b9e7a"]
-  
   vpc_id = "vpc-071c9a3420aa5120a"
   
   worker_groups = [
@@ -19,6 +18,6 @@ module "eks" {
   ]
 }
 
-# output "env-dynamic-url" {
-#   value = module.eks.cluster_endpoint
-# }
+output "env-dynamic-url" {
+   value = module.eks.cluster_endpoint
+}
