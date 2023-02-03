ec58c8f57dc1d40146b398167742af0dae3f5161
hchoi36 <hyunminc77@gmail.com>
2023-02-03 11:26:14 -0500

    commented out version for provider

--- terraform/EKS/main.tf
+++ terraform/EKS/main.tf
@@ -3,7 +3,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(element(concat(data.aws_eks_cluster.cluster[*].certificate_authority.0.data, list("")), 0))
   token                  = element(concat(data.aws_eks_cluster_auth.cluster[*].token, list("")), 0)
   load_config_file       = false
-  version                = "1.9"
+  #version                = "1.9"
 }
 
 module "eks" {
