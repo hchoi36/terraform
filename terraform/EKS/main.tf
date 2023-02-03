8f9fcef7278c496494b061d814caf37e0a8729ce
hchoi36 <hyunminc77@gmail.com>
2023-02-03 10:41:49 -0500

    changed to kube v1.19 and changed worker to 1 cap

--- terraform/EKS/main.tf
+++ terraform/EKS/main.tf
@@ -13,8 +13,8 @@ module "eks" {
       instance_type                 = "t2.small"
       #additional_userdata           = "echo foo bar"
       #additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-      asg_desired_capacity          = 2
-      enable_monitoring = false 
+      asg_desired_capacity          = 1
+      #enable_monitoring = false 
     },
   ]
 }
